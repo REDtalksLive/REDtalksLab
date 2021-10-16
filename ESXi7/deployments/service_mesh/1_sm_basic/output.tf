64f6bf79ebdc8f84b2a1cc7d0372fc29c9856c18
REDtalksLive <info@redtalks.live>
2021-10-16 05:38:16 +0000

    formatting

--- ESXi7/deployments/service_mesh/1_sm_basic/output.tf
+++ ESXi7/deployments/service_mesh/1_sm_basic/output.tf
@@ -1,19 +1,19 @@
 output "servers" {
-    value = { for k, v in esxi_guest.ConsulServers:
-        k => {
-            name = "${v.guest_name}" 
-            ip = "${v.ip_address}",
-        }
+  value = { for k, v in esxi_guest.ConsulServers :
+    k => {
+      name = "${v.guest_name}"
+      ip   = "${v.ip_address}",
     }
+  }
 }
 
 output "clients" {
-    value = { for k, v in esxi_guest.ConsulClients:
-        k => {
-            name = "${v.guest_name}" 
-            ip = "${v.ip_address}",
-        }
+  value = { for k, v in esxi_guest.ConsulClients :
+    k => {
+      name = "${v.guest_name}"
+      ip   = "${v.ip_address}",
     }
+  }
 }
 
 output "ingress_gateway_ip" {
